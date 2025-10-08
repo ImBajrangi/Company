0d51d5be861106ec47056c5fb32e53436f50fe36
ImBajrangi <sharmagharsh1@gmail.com>
2025-10-08 20:27:27 +0530

    Auto update

--- autopushGit.py
+++ autopushGit.py
@@ -8,6 +8,8 @@ from threading import Timer
 
 # --- CONFIG ---
 ENABLE_GIT = True   # 🚦 Set this to False to stop all git commands entirely
+
+
 IGNORED_EXTENSIONS = {".swp", ".tmp", ".log"}
 IGNORED_DIRS = {".vscode", "__pycache__"}
 
