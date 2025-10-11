7e20faea4c83422abaa70b9a3ce347f68266716c
ImBajrangi <sharmagharsh1@gmail.com>
2025-10-11 13:44:19 +0530

    Auto update

--- Vrindopnishad Web/Pictures/js/popup-collection.js
+++ Vrindopnishad Web/Pictures/js/popup-collection.js
@@ -216,11 +216,11 @@ document.addEventListener('DOMContentLoaded', async () => {
         collectionsData = data.collections;
 
         initializePopup();
-        if (collectionsData.featured) generateCollectionItems('featured-slider', collectionsData.featured.items);
-        if (collectionsData.popular) generateCollectionItems('popular-slider', collectionsData.popular.items);
-        if (collectionsData.nature) generateCollectionItems('rapper-slider', collectionsData.nature.items);
-        if (collectionsData.anime) generateCollectionItems('anime-slider', collectionsData.anime.items);
-        if (collectionsData.architecture) generateCollectionItems('architecture-slider', collectionsData.architecture.items);
+        // if (collectionsData.featured) generateCollectionItems('featured-slider', collectionsData.featured.items);
+        // if (collectionsData.popular) generateCollectionItems('popular-slider', collectionsData.popular.items);
+        // if (collectionsData.nature) generateCollectionItems('nature-slider', collectionsData.nature.items);
+        // if (collectionsData.anime) generateCollectionItems('anime-slider', collectionsData.anime.items);
+        // if (collectionsData.architecture) generateCollectionItems('architecture-slider', collectionsData.architecture.items);
     } catch (error) {
         console.error('Error loading collections:', error);
     }
