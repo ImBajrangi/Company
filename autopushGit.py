b69f574d4766c3d6ef4a3ade1f289f652d10788c
ImBajrangi <sharmagharsh1@gmail.com>
2025-10-12 17:37:02 +0530

    Auto update

--- autopushGit.py
+++ autopushGit.py
@@ -6,10 +6,6 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from threading import Timer
 
-
-
-
-
 # --- CONFIG ---
 ENABLE_GIT = True    # 🚦 Set this to False to stop all git commands entirely
 IGNORED_EXTENSIONS = {".swp", ".tmp", ".log"}
