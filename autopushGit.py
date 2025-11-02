cc95e57b607482d56d8abea0ed0dbd6f7108db66
ImBajrangi <sharmagharsh1@gmail.com>
2025-11-02 18:04:54 +0530

    Auto update

--- autopushGit.py
+++ autopushGit.py
@@ -12,6 +12,7 @@ IGNORED_EXTENSIONS = {".swp", ".tmp", ".log", ".env"}
 IGNORED_DIRS = {".vscode", "__pycache__"}
 
 # ---------------
+
 class AutoPush(FileSystemEventHandler):
     def __init__(self):
         self.timer = None
