ff4e81ead88ef9999ddff304ea8f91233e444780
ImBajrangi <sharmagharsh1@gmail.com>
2026-01-31 12:23:17 +0530

    chore: Remove extensive backup and unused web files, and add new Python utility scripts.

--- Vrindopnishad Web/backup/admins code/routes/aboutRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const aboutController = require('../controllers/aboutController');
-
-// Update About Content
-router.put('/', aboutController.updateAbout);
-
-module.exports = router; 
