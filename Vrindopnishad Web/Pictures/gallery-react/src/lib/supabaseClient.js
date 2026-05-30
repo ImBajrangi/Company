c956f539ff1ca9a81aff018eaa78f323372e2e1f
sakhi <sakhi@example.com>
2026-05-30 14:29:33 +0530

    chore(submodule): Link ChitraVrinda submodule

--- Vrindopnishad Web/Pictures/gallery-react/src/lib/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://tilimltxgeucefxzerqi.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRpbGltbHR4Z2V1Y2VmeHplcnFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc2MjQyNTQsImV4cCI6MjA4MzIwMDI1NH0.lwaCJyTRW6jNsfQJ32R_wAwp11yj6bvsJ4fzC0EX_00';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
